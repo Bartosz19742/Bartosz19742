846538fe4310daec4257734aef18e7ac5dfeec4b
Bartosz Pisarek <100776653+Bartosz19742@users.noreply.github.com>
2022-04-07 16:22:45 +0200

    Add files via upload

---
+++ Lab6/controllers.js
@@ -0,0 +1,30 @@
+var portfolioApp = angular.module('portfolioApp',[]); 
+
+portfolioApp.controller('GalleryListCtrl', function($scope) {
+    $scope.galleries = [
+    { 'title':'ziemia1',
+    'when':'2013-08-04', 
+     'thumbnailUrl':'ziemia1.png'
+    },
+    { 'title':'ziemia2',
+    'when':'2015-08-04', 
+     'thumbnailUrl':'ziemia2.png'
+    },
+    { 'title':'ziemia3',
+    'when':'2012-08-04', 
+     'thumbnailUrl':'ziemia3.png'
+    },
+    { 'title':'ziemia4',
+    'when':'2016-08-04', 
+     'thumbnailUrl':'ziemia4.png'
+    },
+    { 'title':'ziemia5',
+    'when':'2014-02-03', 
+     'thumbnailUrl':'ziemia5.png'
+    },
+    { 'title':'ziemia6',
+    'when':'2016-05-03', 
+     'thumbnailUrl':'ziemia6.png'
+    },
+    ];
+    }); 
